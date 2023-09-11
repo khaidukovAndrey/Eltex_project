d68784171e4c2f11d391f0e357dbd2637b3cd390
andrey <andrey@LOS.myguest.virtualbox.org>
2023-09-11 21:35:43 +0700

    Исправил тип функции #714

--- vlan_tagger.c
+++ vlan_tagger.c
@@ -5,13 +5,10 @@
 #include "queue/queue.h"
 
 tag_rules *tag_r;
-Queue *in_queue, *out_queue;
 
 int num_of_rules = 10;
-unsigned char buffer[1522] = {0};
-unsigned char second_buffer[1522] = {0};
 
-unsigned short difine_tag_for_ip(uint32_t addr)
+short difine_tag_for_ip(uint32_t addr)
 {
     if (tag_r == NULL)
     {
