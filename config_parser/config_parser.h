814e59b8076f6d5288ecf6690eb13f484f6eca85
mehamoo <mehamoo@Ubuntu22.myguest.virtualbox.org>
2023-09-13 13:08:25 +0700

    full realization of config parser (#686)

--- config_parser/config_parser.h
+++ config_parser/config_parser.h
@@ -10,17 +10,11 @@ typedef struct tag_rules
     struct in_addr ip_left;
     struct in_addr ip_right;
     int tag;
-} tag_rules;
+} tag_rules_t;
 
-int tag_rules_init(tag_rules **tag_rules_obj, int size);
-void tag_rules_clear(tag_rules **tag_rules_obj);
-int tag_rules_check_collisions(tag_rules *tag_rules_obj, int size);
+int tag_rules_init(tag_rules_t **tag_rules_obj, int size);
+void tag_rules_clear(tag_rules_t **tag_rules_obj);
+int tag_rules_check_collisions(tag_rules_t *tag_rules_obj, int size);
 
 int config_file_check(void);
-int config_file_read(tag_rules *tag_rules_obj);
-
-int ip_converting(struct in_addr *in_addr_obj, const char *ip_str);
-int tag_converting(int *tag_int, const char *tag_str);
-int ip_comparison(struct in_addr ip_1, struct in_addr ip_2);
-int ip_str_to_int(char *ip_str, int *ip_int);
-int char_count(char *str, char symbol);
+int config_file_read(tag_rules_t *tag_rules_obj);
